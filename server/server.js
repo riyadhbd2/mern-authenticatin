c1fb343ff10b751faff6ccc917340d4401610890
riyadhbd2 <riyadhbd2@gmail.com>
2025-02-23 11:34:19 +0100

    cor original updated

--- server/server.js
+++ server/server.js
@@ -12,42 +12,19 @@ const port = process.env.PORT || 6007;
 // call the mongodb connection function
 connectDB();
 
-// const allowedOrigins = [
-//   "https://mern-authenticatin-oe2r-a6rr2srdd-easir-arafats-projects.vercel.app/",
-//   "http://localhost:5174",
-// ];
+const allowedOrigins = "http://localhost:5175";
 
 // middle ware for all api
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-// Allow requests from your client's domain
-const corsOptions = {
-  origin: "https://mern-authenticatin-client.vercel.app", // Replace with your client's URL
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Allow cookies and credentials
-};
-app.use(cors(corsOptions));
 
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         console.log("CORS Origin:", origin);
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("Not allowed by CORS"));
-//         }
-//     },
-//     credentials: true,
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-// }));
 
 // API Endpoints
 app.get("/", (req, res) => {
